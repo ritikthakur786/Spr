7eba9319b5698f83804b0caad081dcde6a8f2ec7
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-04-06 11:44:57 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -7,7 +7,8 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 from pyrogram.enums import ChatType
 
-from config import BOT_USERNAME = odsantispambot
+from config import BOT_USERNAME
+from spr import conn, session, spr
 from spr.core import ikb
 from spr.modules import MODULES
 from spr.utils.misc import once_a_day, once_a_minute, paginate_modules
